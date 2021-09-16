9590c7c2b9fc9ec058ae3e982383ade03a7091e1
Bastien Daugenne <bastien.daugenne@gmail.com>
2021-09-16 09:20:22 +0200

    Exercices CSS-JS Animations

---
+++ CSS-JS-Animations-exercices-BD/Exercice 3/script.js
@@ -0,0 +1,21 @@
+// Evènement JS au click
+
+// function anim(){
+
+//     const changeText  = document.getElementById('text');
+
+//     changeText.addEventListener('click', function(){
+
+//         changeText.classList.toggle('animation');
+
+//     })
+// }
+        
+// anim();   
+
+
+
+
+
+    
+    
